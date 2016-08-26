de56cbcb9e4c5d9136ee86f4bcf2bac98efd9899
csf30816 <elijahporter@zoho.com>
2016-08-26 13:10:13 -0500

    Update scratchapi.js

--- scratchapi.js
+++ scratchapi.js
@@ -1,3 +1,33 @@
+//Require definition start
+var require = (function () {
+    var cache = {};
+    function loadScript(url) {
+        var xhr = new XMLHttpRequest(),
+            fnBody;
+        xhr.open('get', url, false);
+        xhr.send();
+        if (xhr.status === 200 && xhr.getResponseHeader('Content-Type') === 'application/x-javascript') {
+            fnBody = 'var exports = {};\n' + xhr.responseText + '\nreturn exports;';
+            cache[url] = (new Function(fnBody)).call({});
+        }
+    }
+    function resolve(module) {
+        //TODO resolve urls
+        return module;
+    }
+    function require(module) {
+        var url = resolve(module);
+        if (!Object.prototype.hasOwnProperty.call(cache, url)) {
+            loadScript(url);
+        }
+        return cache[url];
+    }
+    require.cache = cache;
+    require.resolve = resolve;
+    return require;
+}());
+//Require end
+
 var https = require('https');
 var net = require('net');
 var util = require('util');
